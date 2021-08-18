bacac50a5f69bb37ce9be038d79d9998d8585aac
oemremolla <osman.mollaalioglu@trendyol.com>
2021-08-18 17:01:13 +0300

    remove default src

--- src/components/GImage/GImage.spec.ts
+++ src/components/GImage/GImage.spec.ts
@@ -22,32 +22,4 @@ describe('GImage', () => {
   it('should render image as default src url when src prop does not set', () => {
     expect(wrapper.vm.src).toEqual('https://cdn.dsmcdn.com/shared/images/trendyol.jpeg');
   });
-
-  it('should render image with given src url', async () => {
-    // given
-    wrapper.setProps({
-      src: 'https://picsum.photos/500?random',
-    });
-
-    // when
-    await wrapper.vm.$nextTick();
-
-    // then
-    expect(wrapper.vm.isSrcUrl).toBeTruthy();
-    expect(wrapper.vm.src).toEqual('https://picsum.photos/500?random');
-  });
-
-  it('should render image with given src absolute path', async () => {
-    // given
-    wrapper.setProps({
-      src: '../../resources/default.jpg',
-    });
-
-    // when
-    await wrapper.vm.$nextTick();
-
-    // then
-    expect(wrapper.vm.isSrcUrl).toBeFalsy();
-    expect(wrapper.vm.src).toEqual('../../resources/default.jpg');
-  });
 });
