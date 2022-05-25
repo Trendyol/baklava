3476702404b156c065c92ffb4293729b2436a373
Murat Çorlu <murat.corlu@trendyol.com>
2022-05-25 14:45:53 +0200

    chore: build script refactored

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,6 @@
 const esbuild = require('esbuild');
 const parseArgs = require('minimist');
+const del = require('del');
 const { litCssPlugin } = require('esbuild-plugin-lit-css');
 
 const args = parseArgs(process.argv.slice(2), {
@@ -8,37 +9,40 @@ const args = parseArgs(process.argv.slice(2), {
 
 (async () => {
   const { globby } = await import('globby');
-
-  const buildOptions = {
-    entryPoints: [
-      'src/grace.ts',
-      ...(await globby([
-        'src/components/**/!(*.(test|d)).ts',
-        'src/themes/*.css',
-      ])),
-    ],
-    loader: {
-      '.woff': 'file',
-      '.woff2': 'file',
-    },
-    outdir: args.serve ? undefined : 'dist',
-    bundle: true,
-    sourcemap: true,
-    format: 'esm',
-    target: 'es2017',
-    splitting: true,
-    metafile: true,
-    minify: true,
-    plugins: [
-      litCssPlugin({
-        filter: /components\/.*\.css$/,
-      }),
-    ],
-  };
+  const destinationPath = 'dist';
 
   try {
+    const buildOptions = {
+      entryPoints: [
+        'src/grace.ts',
+        ...(await globby([
+          'src/components/**/!(*.(test|d)).ts',
+          'src/themes/*.css',
+          'src/components/**/*.svg',
+        ])),
+      ],
+      loader: {
+        '.woff': 'file',
+        '.woff2': 'file',
+        '.svg': 'file',
+      },
+      outdir: args.serve ? undefined : destinationPath,
+      assetNames: 'assets/[name]',
+      bundle: true,
+      sourcemap: true,
+      format: 'esm',
+      target: 'es2017',
+      splitting: true,
+      metafile: true,
+      minify: true,
+      plugins: [
+        litCssPlugin({
+          filter: /components\/.*\.css$/,
+        }),
+      ],
+    };
+
     if (args.serve) {
-      console.log(`serving...`);
       const { host, port } = await esbuild.serve(
         {
           servedir: 'playground',
@@ -46,35 +50,38 @@ const args = parseArgs(process.argv.slice(2), {
         buildOptions
       );
 
-      console.log(`Playground is ready on ${host}:${port}`);
-    } else {
-      const buildResult = await esbuild.build(buildOptions);
+      console.log(`Playground is served on http://${host}:${port}`);
 
-      if (buildResult.errors.length > 0) {
-        console.table(buildResult.errors);
-        console.error('Build Failed!');
-        return;
-      }
-
-      if (buildResult.warnings.length > 0) {
-        console.warn('Warnings:');
-        console.table(buildResult.warnings);
-      }
-
-      const analyzeResult = Object.entries(buildResult.metafile.outputs)
-        .map(([fileName, data]) => ({
-          fileName,
-          size: `${(data.bytes / 1024).toFixed(2)} KB`,
-        }))
-        .filter(
-          ({ fileName }) =>
-            fileName.endsWith('.js') || fileName.endsWith('.css')
-        );
-
-      console.table(analyzeResult, ['fileName', 'size']);
-
-      console.info('Build Done!');
+      return;
     }
+
+    del.sync(destinationPath);
+
+    const buildResult = await esbuild.build(buildOptions);
+
+    if (buildResult.errors.length > 0) {
+      console.table(buildResult.errors);
+      console.error('Build Failed!');
+      return;
+    }
+
+    if (buildResult.warnings.length > 0) {
+      console.warn('Warnings:');
+      console.table(buildResult.warnings);
+    }
+
+    const analyzeResult = Object.entries(buildResult.metafile.outputs)
+      .map(([fileName, data]) => ({
+        fileName,
+        size: `${(data.bytes / 1024).toFixed(2)} KB`,
+      }))
+      .filter(
+        ({ fileName }) => fileName.endsWith('.js') || fileName.endsWith('.css')
+      );
+
+    console.table(analyzeResult, ['fileName', 'size']);
+
+    console.info('Build Done!');
   } catch (error) {
     console.error(error);
     process.exit(1);
