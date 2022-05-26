ae6992680e991a9799c149f604b0771cbc00a951
Murat Çorlu <127687+muratcorlu@users.noreply.github.com>
2022-05-26 13:38:37 +0200

    feat: icon component
    * feat: icon component boilerplate is created
    
    * chore(storybook): use dist build directly in storybook ui
    
    * feat: icon component implemented
    
    * fix: firefox sometimes fails with concurrent tests
    
    * chore: build script refactored
    
    * chore: dependencies are updated
    
    * chore(design): linter fixes
    
    Co-authored-by: Murat Çorlu <murat.corlu@trendyol.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,11 @@
-const path = require('path');
-
 module.exports = {
   logLevel: 'debug',
-  stories: [
-    '../*.md',
-    '../docs/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-  ],
+  stories: ['../*.md', '../docs/**/*.stories.mdx', '../src/**/*.stories.mdx'],
   addons: [
     '@storybook/addon-links',
+    '@storybook/addon-docs',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
-    '@storybook/addon-docs',
   ],
   features: {
     postcss: false,
@@ -19,12 +13,5 @@ module.exports = {
     buildStoriesJson: true,
   },
   framework: '@storybook/web-components',
-  webpackFinal: async config => {
-    const gracePath = path.join(__dirname, '..', 'dist');
-    config.resolve.alias['@trendyol-js/grace'] = path.resolve(
-      gracePath,
-      'grace.js'
-    );
-    return config;
-  },
+  staticDirs: ['../dist'],
 };
