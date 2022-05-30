51ac9d6fbf71d944aeee2240e689bd313b0b7194
Murat Çorlu <127687+muratcorlu@users.noreply.github.com>
2022-05-30 17:09:37 +0200

    feat(icon): svg icon path set automatically for cdn usage (#119)
    * feat(icon): svg icon path set automatically for cdn usage
    
    * ci: remove unneeded js files generated for svgs
    
    Co-authored-by: Murat Çorlu <murat.corlu@trendyol.com>

--- src/grace.ts
+++ src/grace.ts
@@ -1,2 +1,3 @@
 export { default as GrIcon } from './components/icon/gr-icon';
 export { default as GrButton } from './components/gr-button/gr-button';
+export { getIconPath, setIconPath } from './utilities/asset-paths';
