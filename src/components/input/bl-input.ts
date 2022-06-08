8ef051926dccc281841784ebf93e16f55661b250
Murat Çorlu <murat.corlu@trendyol.com>
2022-06-08 15:07:25 +0200

    feat: input work in progress

--- src/components/input/bl-input.ts
+++ src/components/input/bl-input.ts
@@ -1,5 +1,6 @@
 import { CSSResultGroup, html, LitElement, TemplateResult } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, query } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
 
 import style from './bl-input.css';
 
@@ -18,6 +19,8 @@ export default class BlInput extends LitElement {
     return [style];
   }
 
+  @query('input') input: HTMLInputElement;
+
   @property({})
   type: 'text' | 'number' = 'text';
 
@@ -30,12 +33,48 @@ export default class BlInput extends LitElement {
   @property({})
   value = '';
 
+  @property({ type: Number })
+  minlength: number;
+
+  @property({ type: Number })
+  maxlength: number;
+
+  @property({ type: Number })
+  min: number;
+
+  @property({ type: Number })
+  max: number;
+
+  private inputHandler() {
+    this.value = this.input.value;
+    this.input.reportValidity();
+    this.event('bl-input', this.input.value);
+  }
+
+  private changeHandler() {
+    console.log('change', this.input.validity);
+
+    this.event('bl-change', this.input.value);
+  }
+
+  private event(name: string, detail: string) {
+    this.dispatchEvent(
+      new CustomEvent(name, { detail, bubbles: true, composed: true })
+    );
+  }
+
   render(): TemplateResult {
-    return html`<label>${this.label}</label
-      ><input
+    return html`<input
         type=${this.type}
         placeholder="${this.placeholder || this.label}"
-      />`;
+        minlength="${ifDefined(this.minlength)}"
+        maxlength="${ifDefined(this.minlength)}"
+        min="${ifDefined(this.min)}"
+        max="${ifDefined(this.max)}"
+        @change=${this.changeHandler}
+        @input=${this.inputHandler}
+      />
+      <p class="error-message"></p> `;
   }
 }
 
