66ec832ed00ed3acf0f86792942153745f69b20b
Murat Çorlu <127687+muratcorlu@users.noreply.github.com>
2022-07-01 15:30:15 +0200

    feat: input component (#129)
    * feat: input component first boilerplate
    
    * feat: input work in progress
    
    * feat(input): more attributes are defined
    
    * feat(input): set validity with custom validation
    
    * feat(input): styling input
    
    * feat: input error validation and label defined
    
    * feat(input): added input medium features
    
    * feat(input): added input large, added all types
    
    * test(input): some tests are added
    
    * test: fix esbuild plugin causes ts decorators dont run
    
    * test(input): test coverage increased
    
    * refactor(input): linter fixes
    
    * docs(input): story file created
    
    * docs(storybook): adding user interaction test addon
    
    * feat: adding reportValidity method
    
    * docs(storybook): adding basic input ant input labels
    
    * refactor: linter fixes
    
    * fix(input): label-fixed placeholder visibility
    
    * test: include styles in unit tests (#133)
    
    Co-authored-by: Murat Çorlu <murat.corlu@trendyol.com>
    
    * test: clean forgotten tag in test html
    
    * test: dependency fix
    
    * refactor: linter fix
    
    * refactor: linter fix
    
    * fix(storybook): version fixed to 6.5.5
    
    * docs(input): storybook doc improved
    
    * fix(input): css lint fix
    
    * fix(input): css part definition forgotten in the code
    
    * fix(input): font style fixed
    
    * fix(input): ui fixes with new rem sizes
    
    * chore(storybook): multi column demos aligned top
    
    * refactor(input): private properties pointed, better jsdoc
    
    * refactor: better web component analyzer
    
    * fix(input): placeholder color fix
    
    * fix(design): duplicate colors removed
    
    * fix(input): label bg color fixed
    
    Co-authored-by: mehmet.tanas <mehmet.tanas@trendyol.com>

---
+++ src/components/input/bl-input.test.ts
@@ -0,0 +1,143 @@
+import { assert, expect, fixture, oneEvent, html } from '@open-wc/testing';
+import BlInput from './bl-input';
+
+describe('bl-input', () => {
+  const oldFetch = window.fetch;
+
+  before(() => {
+    window.fetch = async (url: RequestInfo | URL) => {
+      if (/.svg$/.test(url.toString())) {
+        return new Response('<svg></svg>');
+      }
+      return new Response('', { status: 404 });
+    };
+  });
+
+  after(() => {
+    window.fetch = oldFetch;
+  });
+
+  it('is defined', () => {
+    const el = document.createElement('bl-input');
+    assert.instanceOf(el, BlInput);
+  });
+
+  it('renders with default values', async () => {
+    const el = await fixture<BlInput>(html`<bl-input></bl-input>`);
+    assert.shadowDom.equal(
+      el,
+      `
+      <input
+        type="text"
+      >
+      <bl-icon
+        class="error-icon"
+        name="alert"
+      >
+      </bl-icon>
+    `
+    );
+  });
+
+  it('should set type number', async () => {
+    const el = await fixture<BlInput>(html`<bl-input type="number"></bl-input>`);
+    expect(el.type).to.equal('number');
+    expect(el.shadowRoot?.querySelector('input')?.getAttribute('type')).to.equal('number');
+  });
+
+  it('should set label', async () => {
+    const labelText = 'Some Label';
+    const el = await fixture<BlInput>(html`<bl-input label="${labelText}"></bl-input>`);
+    const label = el.shadowRoot?.querySelector('label');
+    expect(label).to.exist;
+    expect(label?.innerText).to.equal(labelText);
+  });
+
+  it('should set help text', async () => {
+    const helpText = 'Some help text';
+    const el = await fixture<BlInput>(html`<bl-input help-text="${helpText}"></bl-input>`);
+    const helpMessage = <HTMLParagraphElement>el.shadowRoot?.querySelector('.help-text');
+    expect(helpMessage).to.exist;
+    expect(helpMessage?.innerText).to.equal(helpText);
+  });
+
+  describe('input with icon', () => {
+    it('should show custom icon', async () => {
+      const el = await fixture<BlInput>(html`<bl-input icon="info"></bl-input>`);
+      const customIcon = el.shadowRoot?.querySelector('bl-icon.custom-icon');
+      expect(customIcon).to.exist;
+      expect(customIcon?.getAttribute('name')).to.equal('info');
+    });
+  });
+
+  describe('validation', () => {
+    it('should be valid by default', async () => {
+      const el = await fixture<BlInput>(html`<bl-input></bl-input>`);
+
+      expect(el.validity.valid).to.be.true;
+    });
+
+    it('should be invalid with required attribute', async () => {
+      const el = await fixture<BlInput>(html`<bl-input required></bl-input>`);
+
+      expect(el.validity.valid).to.be.false;
+    });
+
+    it('should set invalid text', async () => {
+      const errorMessage = 'This field is mandatory';
+      const el = await fixture<BlInput>(
+        html`<bl-input required invalid-text="${errorMessage}"></bl-input>`
+      );
+      const errorMessageElement = <HTMLParagraphElement>(
+        el.shadowRoot?.querySelector('.invalid-text')
+      );
+
+      expect(el.validity.valid).to.be.false;
+
+      expect(errorMessageElement).to.exist;
+      expect(errorMessageElement?.innerText).to.equal(errorMessage);
+    });
+
+    it('should show error when reportValidity method called', async () => {
+      const el = await fixture<BlInput>(html`<bl-input required></bl-input>`);
+      el.reportValidity();
+      expect(el.validity.valid).to.be.false;
+      const errorMessageElement = <HTMLParagraphElement>(
+        el.shadowRoot?.querySelector('.invalid-text')
+      );
+      expect(errorMessageElement).to.visible;
+    });
+  });
+
+  describe('events', () => {
+    it('should fire bl-input event when user enters a value', async () => {
+      const el = await fixture<BlInput>(html`<bl-input></bl-input>`);
+      const input = el.shadowRoot?.querySelector('input');
+
+      if (input) {
+        input.value = 'some value';
+      }
+
+      setTimeout(() => input?.dispatchEvent(new Event('input')));
+
+      const ev = await oneEvent(el, 'bl-input');
+      expect(ev).to.exist;
+      expect(ev.detail).to.be.equal('some value');
+    });
+
+    it('should fire bl-input event when input value changes', async () => {
+      const el = await fixture<BlInput>(html`<bl-input></bl-input>`);
+      const input = el.shadowRoot?.querySelector('input');
+
+      if (input) {
+        input.value = 'some value';
+      }
+
+      setTimeout(() => input?.dispatchEvent(new Event('change')));
+
+      const ev = await oneEvent(el, 'bl-change');
+      expect(ev).to.exist;
+      expect(ev.detail).to.be.equal('some value');
+    });
+  });
+});
