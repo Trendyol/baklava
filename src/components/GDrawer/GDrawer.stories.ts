7330cbbb05fe137105c017d1a4f9113e09ce30f3
burak.dagli@trendyol.com <burak.dagli@trendyol.com>
2022-07-19 10:12:36 +0300

    add slot to GDrawer component

--- src/components/GDrawer/GDrawer.stories.ts
+++ src/components/GDrawer/GDrawer.stories.ts
@@ -1,6 +1,7 @@
 import { storiesOf } from '@storybook/vue';
 import GDrawer from './GDrawer.vue';
 import GButton from '../GButton';
+import GSpinner from '../GSpinner';
 
 storiesOf('GDrawer', module)
   .addParameters({ component: GDrawer })
@@ -30,6 +31,35 @@ storiesOf('GDrawer', module)
         >
         </GDrawer>
         <GButton @click="onShow" variant="primary">Open Drawer</GButton>
+      </div>
+    `,
+  }))
+  .add('With Slot', () => ({
+    components: { GDrawer, GButton, GSpinner },
+    data: () => ({
+      show: false,
+    }),
+    methods: {
+      onShow: function () {
+        // @ts-ignore
+        this.show = true;
+      },
+      onClose () {
+        // @ts-ignore
+        this.show = false;
+      }
+    },
+    template: `
+      <div class="g-p-10">
+      <GDrawer
+        title="Help"
+        :show="show"
+        @close="onClose"
+        top=0
+      >
+        <GSpinner/>
+      </GDrawer>
+      <GButton @click="onShow" variant="primary">Open Drawer</GButton>
       </div>
     `,
   }))
