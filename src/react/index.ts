412fb2aba6f1b44bcb521a5c1bed935c13cdac8b
Enes Başpınar <enes.baspinar@trendyol.com>
2022-08-02 13:25:54 +0300

    fix: linter

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,26 +1,26 @@
-import React from "react";
-import { createComponent } from "@lit-labs/react";
-import BlIcon from "../components/icon/bl-icon";
-import BlButton from "../components/button/bl-button";
-import BlInput from "../components/input/bl-input";
-import BlBadge from "../components/badge/bl-badge";
-import BlTooltip from "../components/tooltip/bl-tooltip";
+import React from 'react';
+import { createComponent } from '@lit-labs/react';
+import BlIcon from '../components/icon/bl-icon';
+import BlButton from '../components/button/bl-button';
+import BlInput from '../components/input/bl-input';
+import BlBadge from '../components/badge/bl-badge';
+import BlTooltip from '../components/tooltip/bl-tooltip';
 
-const _BlIcon = createComponent(React, "bl-icon", BlIcon, {
-  onLoad: "bl-load",
-  onError: "bl-error",
+const _BlIcon = createComponent(React, 'bl-icon', BlIcon, {
+  onLoad: 'bl-load',
+  onError: 'bl-error',
 });
-const _BlButton = createComponent(React, "bl-button", BlButton, {
-  onClick: "bl-click",
+const _BlButton = createComponent(React, 'bl-button', BlButton, {
+  onClick: 'bl-click',
 });
-const _BlInput = createComponent(React, "bl-input", BlInput, {
-  onChange: "bl-change",
-  onInput: "bl-input",
+const _BlInput = createComponent(React, 'bl-input', BlInput, {
+  onChange: 'bl-change',
+  onInput: 'bl-input',
 });
-const _BlBadge = createComponent(React, "bl-badge", BlBadge, {});
-const _BlTooltip = createComponent(React, "bl-tooltip", BlTooltip, {
-  onShow: "bl-tooltip-show",
-  onHide: "bl-tooltip-hide",
+const _BlBadge = createComponent(React, 'bl-badge', BlBadge, {});
+const _BlTooltip = createComponent(React, 'bl-tooltip', BlTooltip, {
+  onShow: 'bl-tooltip-show',
+  onHide: 'bl-tooltip-hide',
 });
 
 export { _BlIcon as BlIcon };
