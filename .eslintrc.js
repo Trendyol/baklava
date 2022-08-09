165413d506c41eca864e1ceb4cf50f564396f77b
Murat Çorlu <flanges-slash0t@icloud.com>
2022-08-09 15:55:07 +0200

    chore: pipeline and commit hook linters are synced

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,13 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
   extends: [
-    'prettier',
+    // 'prettier',
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
   ],
-  plugins: ['@typescript-eslint'],
   env: {
     browser: true,
   },
@@ -23,21 +19,18 @@ module.exports = {
   },
   overrides: [
     {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'plugin:@typescript-eslint/recommended'
+      ],
+      plugins: ['@typescript-eslint'],
+    },
+    {
       files: ['rollup.config.js', 'web-test-runner.config.js'],
       env: {
         node: true,
       },
-    },
-    {
-      files: [
-        '*.test.ts',
-        '**/custom_typings/*.ts',
-        'packages/lit-ssr/src/test/integration/tests/**',
-        'packages/lit-ssr/src/lib/util/parse5-utils.ts',
-      ],
-      rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-      },
-    },
+    }
   ],
 };
