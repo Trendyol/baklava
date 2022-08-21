9f7208646e70aac775d0bb886d590ff826d4d7fe
Ali Balbars <ali.balbars@trendyol.com>
2022-08-21 21:36:26 +0300

    refactor(checkbox): refactor for checkbox and checkbox group

--- src/components/checkbox-group/checkbox/bl-checkbox.ts
+++ src/components/checkbox-group/checkbox/bl-checkbox.ts
@@ -1,12 +1,22 @@
 import { CSSResultGroup, html, LitElement, TemplateResult } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { event, EventDispatcher } from '../../../utilities/event';
 import style from './bl-checkbox.css';
 import type BlCheckboxGroup from '../bl-checkbox-group';
-import { event, EventDispatcher } from '../../../utilities/event';
 
 /**
  * @tag bl-checkbox
  * @summary Baklava Checkbox component
+ *
+ * @property {string} label - Sets the label for checkbox
+ * @property {boolean} checked - Sets the checked state for checkbox
+ * @property {boolean} disabled - Sets the disabled state for checkbox
+ *
+ * @cssproperty --bl-color-primary - Sets the color of checkbox label and box.
+ * @cssproperty --bl-color-tertiary - Sets the color of disabled checkbox box.
+ * @cssproperty --bl-color-content-passive - Sets the color of disabled checkbox icon.
+ * @cssproperty --bl-font-size-2xs - Sets the icon font size.
  */
 
 @customElement('bl-checkbox')
@@ -14,13 +24,14 @@ export default class BlCheckbox extends LitElement {
   static get styles(): CSSResultGroup {
     return [style];
   }
+
   private checkboxGroup: BlCheckboxGroup | null;
 
   @query('#checkbox')
-  checkbox: any;
+  checkbox: HTMLInputElement;
 
   @query('#label')
-  labelEl: any;
+  labelEl: HTMLLabelElement;
 
   @property({ type: String, reflect: true })
   label = '';
@@ -34,56 +45,54 @@ export default class BlCheckbox extends LitElement {
   @property({ type: Boolean, reflect: true })
   indeterminate = false;
 
-  @event('bl-checkbox-change') private onChange: EventDispatcher<boolean>;
+  @property({ type: Boolean })
+  indeterminateAllowed = false;
 
-  iconNames = {
-    checked: 'check',
-    indeterminate: 'minus',
-  };
+  @event('bl-checkbox-change') private onChange: EventDispatcher<boolean>;
 
   icons = {
     checked: html`<bl-icon class="icon" name="check"></bl-icon>`,
     indeterminate: html`<bl-icon class="icon" name="minus"></bl-icon>`,
   };
 
-  handleChange(event: any) {
-    this.checked = event.target.checked;
-    this.onChange(event.target.checked);
-
-    if (this.indeterminate) {
-      this.indeterminate = false;
-    }
+  handleChange(event: CustomEvent) {
+    const target = event.target as HTMLInputElement;
+    this.checked = target.checked;
+    this.onChange(target.checked);
+    this.indeterminate = false;
   }
 
-  updated(changedProperties: Map<string, unknown>) {
-    if (this.indeterminate) {
+  updated() {
+    this.checkbox.checked = this.checked;
+
+    // send slot elements change event to parent
+    this.checkboxGroup = this.closest<BlCheckboxGroup>('bl-checkbox-group');
+    this.checkboxGroup?.childCheckboxChanged();
+
+    if (this.indeterminate && this.indeterminateAllowed) {
       this.checkbox.indeterminate = true;
-    } else {
-      this.checkbox.indeterminate = false;
-    }
-
-    if (changedProperties.has('checked') && !this.disabled) {
-      const checkedProp = changedProperties.get('checked');
-      const propChecked = typeof checkedProp === 'undefined' ? false : !checkedProp;
-      this.checkbox.checked = propChecked;
-
-      // send slot change event to parent
-      this.checkboxGroup = this.closest<BlCheckboxGroup>('bl-checkbox-group');
-      this.checkboxGroup?.childCheckboxChanged();
     }
   }
 
   render(): TemplateResult {
     return html`
     <label id="label" class="label">
-      <input id="checkbox" type="checkbox" name="checkbox" ?checked=${this.checked} ?disabled=${
-      this.disabled
-    } @change=${this.handleChange} />
+      <input
+        id="checkbox"
+        type="checkbox"
+        name="checkbox"
+        ?checked=${this.checked}
+        ?disabled=${this.disabled}
+        @change=${this.handleChange} />
       <div class="box">
-        ${this.indeterminate && !this.checked ? this.icons.indeterminate : null}
         ${this.checked ? this.icons.checked : null}
+        ${
+          this.indeterminate && this.indeterminateAllowed && !this.checked
+            ? this.icons.indeterminate
+            : null
+        }
       </div>
-      <span class="text">${this.label}</span>
+      ${ifDefined(this.label) && html`<span class="text">${this.label}</span>`}
     </label>
     `;
   }
