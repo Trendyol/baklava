8ded0067e352fc475e271254e0352ede68b0a17d
Doğucan Durmuşlar <dogucan.durmuslar@trendyol.com>
2022-09-02 13:19:50 +0300

    feat(alert): attribute name & html refactored

--- src/components/alert/bl-alert.ts
+++ src/components/alert/bl-alert.ts
@@ -5,13 +5,16 @@ import style from './bl-alert.css';
 import '../icon/bl-icon';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { iconConverter } from './icon.converter';
-import { ButtonVariant, ButtonKind } from '../button/bl-button';
+import { ButtonVariant, ButtonKind, ButtonSize } from '../button/bl-button';
 
 export type AlertVariant = 'info' | 'warning' | 'success' | 'error';
 
 /**
  * @tag bl-alert
  * @summary Baklava Alert component
+ *
+ * @cssproperty --bl-alert-display - Sets the display property of button. Default value is 'inline-block'.
+ *
  */
 
 @customElement('bl-alert')
@@ -23,8 +26,8 @@ export default class BlAlert extends LitElement {
   @property()
   variant: AlertVariant = 'info';
 
-  @property({ attribute: 'alert-description' })
-  alertDescription: 'string';
+  @property()
+  description: 'string';
 
   @property({ converter: iconConverter() })
   icon?: boolean | string;
@@ -32,20 +35,20 @@ export default class BlAlert extends LitElement {
   @property({type: Boolean})
   closable = false;
 
-  @property({ attribute: 'alert-title' })
-  alertTitle?: string;
+  @property()
+  caption?: string;
 
   @property({ type: Boolean, reflect: true })
   hidden = false;
 
   @event('bl-close') private onClose: EventDispatcher<boolean>;
 
-  get _hasAlertTitleSlot() {
-    return this.querySelector(':scope > [slot="alert-title"]') !== null;
+  get _hasAlertCaptionSlot() {
+    return this.querySelector(':scope > [slot="caption"]') !== null;
   }
 
   get _getAlertActionSlot() {
-    return this.querySelector(':scope > [slot="alert-action"]');
+    return this.querySelector(':scope > [slot="action"]');
   }
 
   closeHandler() {
@@ -83,24 +86,24 @@ export default class BlAlert extends LitElement {
     if(actionSlot?.tagName !== 'BL-BUTTON') throw new Error("Action slot must contain bl-button component as child!");
     actionSlot.setAttribute('variant','secondary' as ButtonVariant);
     actionSlot.setAttribute('kind','text' as ButtonKind);
+    actionSlot.setAttribute('size','medium' as ButtonSize);
     actionSlot.removeAttribute('icon');
   }
 
   render(): TemplateResult {
     this.initAlertActionSlot();
-    const titleTemp = html`<span class="title"><slot name="alert-title">${this.alertTitle}</slot></span>`;
+    const captionTemp = html`<span class="caption"><slot name="caption">${this.caption}</slot></span>`;
     const iconTemp = html`<bl-icon class="icon" name=${ifDefined(this.getIcon())}></bl-icon>`;
     const closableTemp = html`<bl-button kind="text" icon="close" variant="secondary" @click=${this.closeHandler}></bl-button>`;
 
-    const title = this.shouldRender((this.alertTitle || this._hasAlertTitleSlot), titleTemp);
+    const caption = this.shouldRender((this.caption || this._hasAlertCaptionSlot), captionTemp);
     const icon = this.shouldRender(this.getIcon(), iconTemp);
     const closable = this.shouldRender(this.closable, closableTemp);
-
-    const actionsTemp = html `
-      <div class="actions">
-        ${closable}
-      </div>`;
-    const actions = this.shouldRender(!!closable, actionsTemp);
+    const description = html`<span class="description">
+      <slot>
+        ${this.description}
+      </slot>
+    </span>`
 
     return html`
       <div class="alert">
@@ -108,17 +111,13 @@ export default class BlAlert extends LitElement {
           <div class="content">
             ${icon}
             <div class="text-content">
-              ${title}
-              <span class="description">
-                <slot>
-                  ${this.alertDescription}
-                </slot>
-              </span>
+              ${caption}
+              ${description}
             </div>
           </div>
-          <slot name="alert-action"></slot>
+          <slot name="action"></slot>
         </div>
-        ${actions}
+        ${closable}
       </div>
     `;
   }
