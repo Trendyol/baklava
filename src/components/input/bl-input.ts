842a2dcec91b129a217f0d2ebe03aa34bc7b8332
Murat Çorlu <127687+muratcorlu@users.noreply.github.com>
2023-01-19 16:40:06 +0100

    fix(input): reflection of attributes used in styles (#379)

--- src/components/input/bl-input.ts
+++ src/components/input/bl-input.ts
@@ -37,7 +37,7 @@ export default class BlInput extends FormControlMixin(LitElement) {
   /**
    * Sets label of the input
    */
-  @property({})
+  @property({ reflect: true })
   label?: string;
 
   /**
@@ -103,7 +103,7 @@ export default class BlInput extends FormControlMixin(LitElement) {
   /**
    * Makes label as fixed positioned
    */
-  @property({ type: Boolean, attribute: 'label-fixed' })
+  @property({ type: Boolean, attribute: 'label-fixed', reflect: true })
   labelFixed = false;
 
   /**
