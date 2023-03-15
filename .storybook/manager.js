0465c5b15808f53f2903811c251fc2a758c5ea0e
Murat Çorlu <murat.corlu@trendyol.com>
2023-03-15 09:05:52 +0100

    feat: generic typography styles are added

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,6 +5,7 @@ const theme = create({
   base: 'light',
   brandTitle: 'Baklava Design System',
   brandUrl: 'https://github.com/Trendyol/baklava',
+  brandImage: 'https://user-images.githubusercontent.com/127687/194415334-0dc8fbf2-3e87-44ed-b23a-0cc9da767b11.png',
   colorPrimary: '#F27A1A',
   colorSecondary: '#273142',
   fontBase: 'RubikVariable, sans-serif',
@@ -12,4 +13,26 @@ const theme = create({
     '"Operator Mono","Fira Code Retina","Fira Code","FiraCode-Retina","Andale Mono","Lucida Console",Consolas,Monaco,monospace',
 });
 
-addons.setConfig({ theme });
+addons.setConfig({
+  // isFullscreen: true,
+  // showNav: true,
+  // showPanel: true,
+  // panelPosition: 'bottom',
+  // enableShortcuts: true,
+  // showToolbar: true,
+  // selectedPanel: undefined,
+  // initialActive: 'sidebar',
+  // sidebar: {
+  //   showRoots: false,
+  //   collapsedRoots: ['other'],
+  // },
+  // toolbar: {
+  //   title: { hidden: false },
+  //   zoom: { hidden: false },
+  //   eject: { hidden: false },
+  //   copy: { hidden: false },
+  //   fullscreen: { hidden: false },
+  // },
+
+  theme
+});
