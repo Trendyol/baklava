7d225ae5513ada268c0e9717af8002271eab3450
Murat Çorlu <murat.corlu@trendyol.com>
2023-03-31 00:53:16 +0200

    docs: logo for storybook

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,7 @@ const theme = create({
   base: 'light',
   brandTitle: 'Baklava Design System',
   brandUrl: 'https://github.com/Trendyol/baklava',
-  brandImage: 'https://user-images.githubusercontent.com/127687/194415334-0dc8fbf2-3e87-44ed-b23a-0cc9da767b11.png',
+  brandImage: 'https://user-images.githubusercontent.com/127687/228982173-df396f7f-9f1e-4b40-a96c-15aecc9ae344.png',
   colorPrimary: '#F27A1A',
   colorSecondary: '#273142',
   fontBase: 'RubikVariable, sans-serif',
