3cec539a9365d8489732006d532056f5a17b2d62
Enes Yıldırım <enes.yildirim@trendyol.com>
2023-07-14 14:01:52 +0300

    feat(tooltip): add css variable for set display of tooltip trigger

--- src/components/tooltip/bl-tooltip.ts
+++ src/components/tooltip/bl-tooltip.ts
@@ -13,7 +13,7 @@ import style from './bl-tooltip.css';
  * @summary Baklava Tooltip component
  * @dependency bl-popover
  *
- * @property {string} placement - Sets the tooltip placement
+ * @cssproperty [--bl-tooltip-trigger-display=inline-flex] Set the display of the tooltip trigger.
  */
 @customElement('bl-tooltip')
 export default class BlTooltip extends LitElement {
