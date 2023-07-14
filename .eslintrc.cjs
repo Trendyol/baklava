7bb793fcd44e5534d267f5c3198a43033fcbafa2
Aykut Saraç <aykut.sarac@trendyol.com>
2023-07-14 16:51:47 +0300

    chore: update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,28 +2,59 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  extends: ['eslint:recommended', 'prettier', 'plugin:storybook/recommended'],
+  extends: [
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+    "eslint:recommended",
+    "plugin:storybook/recommended",
+  ],
+  plugins: ["prettier", "unused-imports", "@typescript-eslint"],
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   globals: {
-    process: 'readonly'
+    process: "readonly",
   },
   rules: {
-    strict: ['error', 'never']
+    "prettier/prettier": "error",
+    "strict": ["error", "never"],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "double", { avoidEscape: true }],
+    "semi": ["error", "always"],
+    "no-empty": "off",
+    "unused-imports/no-unused-imports": "error",
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+      { blankLine: "any", prev: ["const", "let", "var"], next: ["const", "let", "var"] },
+    ],
+    "space-in-parens": "error",
+    "no-multiple-empty-lines": "error",
+    "no-irregular-whitespace": "error",
+    "space-before-function-paren": [
+      "error",
+      {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always",
+      },
+    ],
   },
-  overrides: [{
-    files: ['*.ts'],
-    parser: '@typescript-eslint/parser',
-    extends: ['plugin:@typescript-eslint/recommended'],
-    plugins: ['@typescript-eslint']
-  }, {
-    files: ['web-test-runner.config.js'],
-    env: {
-      node: true
-    }
-  }]
+  overrides: [
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      extends: ["plugin:@typescript-eslint/recommended"],
+      plugins: ["@typescript-eslint"],
+    },
+    {
+      files: ["web-test-runner.config.js"],
+      env: {
+        node: true,
+      },
+    },
+  ],
 };
