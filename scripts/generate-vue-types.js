962addc78f14d23a54414c3b8661055cdf424333
AykutSarac <aykutsarac0@gmail.com>
2023-10-03 10:11:27 +0300

    fix(vue): missing component types

--- scripts/generate-vue-types.js
+++ scripts/generate-vue-types.js
@@ -1,22 +1,18 @@
 import fs from "fs";
 
-const components = fs.readdirSync("./src/components");
-const blComponents = components.map(convertToComponentName);
+const components = JSON.parse(fs.readFileSync("./dist/custom-elements.json", "utf-8"));
+const declerations = components.modules.flatMap(module => module.declarations);
+const customElements = declerations.filter(declaration => declaration.customElement === true);
+const customElementNames = customElements.map(customElement => customElement.name);
 
 const code = `
 import type * as Baklava from '@trendyol/baklava/dist/baklava'
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ${blComponents.map(component => `${component}: import("vue").Component<Baklava.${component}>`).join('\n    ')}
+    ${customElementNames.map(component => `${component}: import("vue").Component<Baklava.${component}>`).join('\n    ')}
   }
 }
 `
 
 fs.writeFileSync("./dist/baklava-vue.d.ts", code);
-
-function convertToComponentName(componentName) {
-  const words = componentName.split("-");
-  const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
-  return `Bl${capitalizedWords.join("")}`;
-}
