f1e85a0de67c5193b65e51f451fd123cede07d4d
Aykut Saraç <aykut.sarac@trendyol.com>
2024-01-30 15:18:46 +0300

    feat(storybook): add docs & listen for lang change

--- src/localization.ts
+++ src/localization.ts
@@ -19,6 +19,31 @@ export const init = async () => {
   if (htmlLang && allLocales.includes(htmlLang)) {
     await setLocale(htmlLang);
   }
+
+  // Add event listener for 'lang' attribute changes
+  const langAttributeChangeListener = (mutations: MutationRecord[]) => {
+    mutations.forEach(mutation => {
+      if (mutation.attributeName === "lang") {
+        const newLangValue = html?.getAttribute("lang") as LangKey | null;
+
+        if (newLangValue && allLocales.includes(newLangValue)) {
+          setLocale(newLangValue);
+        }
+      }
+    });
+  };
+
+  // Check if MutationObserver is supported
+  if (typeof MutationObserver !== "undefined") {
+    const observer = new MutationObserver(langAttributeChangeListener);
+
+    observer.observe(html as Node, { attributes: true });
+  } else {
+    // Fallback to DOMAttrModified for older browsers
+    html?.addEventListener("DOMAttrModified", e =>
+      langAttributeChangeListener([e as unknown as MutationRecord])
+    );
+  }
 };
 
 export default init();
