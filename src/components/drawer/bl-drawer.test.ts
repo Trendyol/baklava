aef4f6ee8825ce0f62cbb69463a9af29d49ce255
Erdem Gönül <ahmeterdem.gonul@trendyol.com>
2024-04-01 15:18:05 +0300

    feat(drawer): add dynamic width feature to drawer

--- src/components/drawer/bl-drawer.test.ts
+++ src/components/drawer/bl-drawer.test.ts
@@ -140,7 +140,53 @@ describe("bl-drawer", () => {
 
       expect(el?.shadowRoot?.querySelector("bl-button")).to.be.null;
     });
+
+    it("should render the drawer with given specific width in css variable --bl-drawer-width", async () => {
+        document.documentElement.style.setProperty("--bl-drawer-width", "800px");
+        const el = await fixture<typeOfBlDrawer>(html`<bl-drawer open></bl-drawer>`);
+        const drawerEl = el.shadowRoot!.querySelector(".drawer")!;
+
+        const width = getComputedStyle(drawerEl).width;
+
+        expect(
+          width
+        ).to.equal("800px");
+      });
+
+    it("should render the drawer with width: 'calc(100vw - 24px)' if viewport width is smaller than given drawer width", async () => {
+        // default window.innerWidth = 800px
+        document.documentElement.style.setProperty("--bl-drawer-width", "1200px");
+
+        const el = await fixture<typeOfBlDrawer>(html`<bl-drawer open></bl-drawer>`);
+        const drawerEl = el.shadowRoot!.querySelector(".drawer")!;
+
+        const width = getComputedStyle(drawerEl).width;
+
+        expect(
+          width
+        ).to.equal("776px");
+
+      });
+
+      it("should render the drawer with default width 'calc(100vw - 24px)' if --bl-drawer-width value is unsupported & viewport width is smaller than given drawer width", async () => {
+        Object.defineProperty(window, "innerWidth", {
+            writable: true,
+            configurable: true,
+            value: 300,
+        });
+        document.documentElement.style.setProperty("--bl-drawer-width", "500em");
+
+        const el = await fixture<typeOfBlDrawer>(html`<bl-drawer open></bl-drawer>`);
+        const drawerEl = el.shadowRoot!.querySelector(".drawer")!;
+
+        const width = getComputedStyle(drawerEl).width;
+
+        expect(
+          width
+        ).to.equal("776px");
+      });
   });
+
   describe("event tests", () => {
     it("should fire bl-drawer-open when dialog opens", async () => {
       const el = await fixture<typeOfBlDrawer>(html`<bl-drawer caption="My Drawer"></bl-drawer>`);
