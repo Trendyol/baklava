6609f3b14fe5fb9384ed3e62262c1325c2714a70
ogunb <ogun.babacan@trendyol.com>
2024-07-29 19:59:21 +0300

    refactor(icon): implement baklava-icons and remove icons folder
    Also ignored examples workspace as it blocks commiting without installing in each project.
    
    Closes #530

--- examples/input-mask-vue/src/main.js
+++ examples/input-mask-vue/src/main.js
@@ -1,15 +1,17 @@
 import { createApp } from 'vue';
 import { setIconPath } from '@trendyol/baklava';
 
-setIconPath('https://cdn.jsdelivr.net/npm/@trendyol/baklava@3.0.0/dist/assets');
+setIconPath('https://cdn.jsdelivr.net/npm/@trendyol/baklava-icons@latest/icons');
 
 import App from './App.vue';
+
 import router from './router';
 
 import '@trendyol/baklava/dist/themes/default.css';
 import './style.css';
 
-const app = createApp(App);
+const app =
+createApp(App)
 
 app.use(router);
 
