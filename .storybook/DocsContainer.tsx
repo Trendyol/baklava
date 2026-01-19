9195f5155d7a523eca317038039989d4c5341e9d
Ergenekon Yigit <ergenekon.yigit@trendyol.com>
2026-01-19 14:43:37 +0300

    refactor: remove dark theme from Storybook and streamline theme management

--- .storybook/DocsContainer.tsx
+++ .storybook/DocsContainer.tsx
@@ -1,8 +1,7 @@
-import * as React from 'react';
 import { DocsContainer as BaseContainer, DocsContainerProps } from '@storybook/blocks';
 import { create, themes } from '@storybook/theming';
+import * as React from 'react';
 
-// Custom dark theme with custom background color
 const customDarkTheme = create({
   ...themes.dark,
   base: 'dark',
@@ -12,12 +11,10 @@ const customDarkTheme = create({
 });
 
 export const DocsContainer: React.FC<React.PropsWithChildren<DocsContainerProps>> = ({ children, context }) => {
-  // Get theme from context globals
   const currentTheme = context?.store?.globals?.globals?.theme || 'light';
   const [theme, setTheme] = React.useState(currentTheme);
 
   React.useEffect(() => {
-    // Update theme when context changes
     setTheme(currentTheme);
   }, [currentTheme]);
 
--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,7 +1,7 @@
 import { addons } from "@storybook/manager-api";
 import { lightTheme } from "./baklava-theme";
 
-// Set static light theme for Storybook UI
+// Static light theme for Storybook UI
 // Dark mode only affects the iframe preview, not the sidebar/toolbar
 addons.setConfig({
   theme: lightTheme,
